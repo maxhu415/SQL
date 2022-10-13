cd1b1a66b4eb57179006da4c8be99229cd89717c
Max Hu <78884719+Limitless415@users.noreply.github.com>
2022-10-13 17:39:18 -0500

    Update Triggers.sql

--- Phase 5 - PLSQL/Triggers.sql
+++ Phase 5 - PLSQL/Triggers.sql
@@ -1,6 +1,3 @@
-@"C:\Users\Maxhu\Documents\School work\Fall 2021\Database Models 4125\Phase 3\Hu_schema.sql"
-@"C:\Users\Maxhu\Documents\School work\Fall 2021\Database Models 4125\Phase 3\Hu_Inserts.sql"
-
 SELECT * FROM Agent1;
 SELECT * FROM House;
 
